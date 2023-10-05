66e7069cc7cc1fbade5ab985039d2ff4e0371007
Dauren <dauren.ktl@gmail.com>
2023-10-05 19:08:15 +0600

    create the project

---
+++ SpyInAction/SpyInAction.h
@@ -0,0 +1,18 @@
+//
+//  SpyInAction.h
+//  SpyInAction
+//
+//  Created by Dauren Muratov on 05.10.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpyInAction.
+FOUNDATION_EXPORT double SpyInActionVersionNumber;
+
+//! Project version string for SpyInAction.
+FOUNDATION_EXPORT const unsigned char SpyInActionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpyInAction/PublicHeader.h>
+
+
---
+++ SpyInActionTests/SpyInActionTests.swift
@@ -0,0 +1,12 @@
+//
+//  SpyInActionTests.swift
+//  SpyInActionTests
+//
+//  Created by Dauren Muratov on 05.10.2023.
+//
+
+import XCTest
+
+final class SpyInActionTests: XCTestCase {
+
+}
