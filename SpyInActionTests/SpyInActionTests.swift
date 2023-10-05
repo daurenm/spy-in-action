c5978d64cfbb9a797682bc6c9bbb78cf39f3ec57
Dauren <dauren.ktl@gmail.com>
2023-10-05 19:42:28 +0600

    test init does not request data from client

--- SpyInActionTests/SpyInActionTests.swift
+++ SpyInActionTests/SpyInActionTests.swift
@@ -6,7 +6,31 @@
 //
 
 import XCTest
+import SpyInAction
+
+class RemotePostsLoader {
+    let client: HTTPClient
+    init(client: HTTPClient) {
+        self.client = client
+    }
+}
 
 final class SpyInActionTests: XCTestCase {
 
+    func test_init_doesNotRequestDataFromClient() {
+        let client = HTTPClientSpy()
+        _ = RemotePostsLoader(client: client)
+        
+        XCTAssertTrue(client.messages.isEmpty)
+    }
+    
+    // MARK: - Helpers
+    
+    private class HTTPClientSpy: HTTPClient {
+        private(set) var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
+    
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+            messages.append((url, completion))
+        }
+    }
 }
